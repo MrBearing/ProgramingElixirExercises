5483fcb5715a5287f607ab90b5ae7277140771ad
Takumi Okamoto <takumi1988okamoto@gmail.com>
2020-03-30 23:45:29 +0900

    #9 練習問題9のためにテストファイル追加

---
+++ lib/ch14/WorkingWithMultipleProcess09/find_cat.exs
@@ -0,0 +1,91 @@
+# defmodule  FibSolver do
+#   def fib(scheduler) do
+#     send scheduler, {:ready, self()}
+#     receive do
+#       {:find_cat,n ,client} ->
+#         send client,{:answer , n ,fib_calc(n), self()}
+#         fib( scheduler)
+#       {:shutdown} ->
+#         exit(:nomal)
+#     end
+#   end
+
+#   defp fib_calc(0), do: 0
+#   defp fib_calc(1), do: 1
+#   defp fib_calc(n), do: fib_calc(n-1) + fib_calc(n-2)
+# end
+
+defmodule WordCounter do
+  def count_word(scheduler) do
+    send scheduler, {:ready, self()}
+    receive do
+      {:find_cat,file_name,,client} ->
+        send client,{:answer , n ,count(n), self()}
+
+        count_word( scheduler)
+      {:shutdown} ->
+        exit(:nomal)
+    end
+  end
+
+  def
+
+  def count_word(file_str, search_string) do
+    r = Regex.compile!(search_string)
+    Regex.scan(r ,file_str) |> length
+  end
+
+end
+
+defmodule Scheduler do
+
+  def run(num_process, module, func ,  ) do
+    (1..num_process)
+    |> Enum.map(fn(_) -> spawn(module, func, [self()]) end)
+    |> schedule_processes(to_calculate, [])
+  end
+
+  def schedule_processes(processes, queue, results)do
+    receive do
+      {:ready, pid} when length(queue) > 0 ->
+        [next | tail] = queue
+        send pid, {:count_word, next, self()}
+        schedule_processes(processes, tail, results)
+      {:ready, pid} ->
+        send pid, {:shutdown}
+        if length(processes) > 1 do
+          schedule_processes(List.delete(processes,pid), queue, results)
+        else
+          Enum.sort(results,fn {n1,_}, {n2,_} -> n1 <= n2 end)
+        end
+      {:answer, number, result, _pid }->
+        schedule_processes(processes, queue, [{number, result} | results])
+    end
+  end
+end
+
+to_process = [37, 37, 37, 37, 37, 37 ]
+
+Enum.each 1..16, fn num_processes ->
+  {time, result} = :timer.tc(
+    Scheduler, :run,
+    [num_processes, WordCounter, :count_word , to_process]
+  )
+  if num_processes == 1 do
+    IO.puts inspect result
+    IO.puts "\n #   time(s)"
+  end
+  :io.format "~2B      ~.2f~n",[num_processes, time/1000000.0]
+end
+
+"""
+文字列のカウントは
+Regex.scan(~r{"\id\":}, s) |> length
+r = Regex.compile!("cat")
+Regex.scan(r ,test) |> length
+を使えば良さそう。
+
+乱暴だけど、
+|> String.split("id") |> length - 1
+このパターンも
+"""
