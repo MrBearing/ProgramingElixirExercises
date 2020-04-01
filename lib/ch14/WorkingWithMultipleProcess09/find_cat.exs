a6c34050dca34fa49489f3ee459b40aa1b7824e5
Takumi Okamoto <takumi1988okamoto@gmail.com>
2020-04-02 00:33:35 +0900

    #9 練習問題9回答完了！！　14章終了。

--- lib/ch14/WorkingWithMultipleProcess09/find_cat.exs
+++ lib/ch14/WorkingWithMultipleProcess09/find_cat.exs
@@ -19,58 +19,61 @@ defmodule WordCounter do
   def count_word(scheduler) do
     send scheduler, {:ready, self()}
     receive do
-      {:count_word, n, file_name, search_string,client} ->
-        send client,{:answer , n ,count(n), self()}
-        count_word( scheduler)
+      {:count_word, file_name, search_string, client} ->
+        send client,{:answer , count(file_name, search_string), self()}
+        count_word(scheduler)
       {:shutdown} ->
         exit(:nomal)
     end
   end
 
-  def
-
-  def count_(file_path, search_string) do
+  def count(file_path, search_string) do
     file_str = File.read!(file_path)
     r = Regex.compile!(search_string)
-    Regex.scan(r ,file_str) |> length
+    Regex.scan(r ,file_str)
+    |> length
   end
 
 end
 
 defmodule Scheduler do
 
-  def run(num_process, module, func , files) do
+  def run(num_process, module, func , queue ,arg) do
     (1..num_process)
     |> Enum.map(fn(_) -> spawn(module, func, [self()]) end)
-    |> schedule_processes(to_calculate, [])
+    |> schedule_processes(queue,arg,[])
   end
 
-  def schedule_processes(processes, queue, results)do
+  # もう少し汎用的なスケジューラーを作りたい。。。
+  # 引数を工夫することでできないだろうか。。
+  def schedule_processes(processes, queue , word , results)do
     receive do
-      {:ready, pid} when length(queue) > 0 ->
+      {:ready, pid} when queue != [] ->
         [next | tail] = queue
-        send pid, {:count_word, next, self()}
-        schedule_processes(processes, tail, results)
+        send pid, {:count_word, next, word, self()}
+        schedule_processes(processes, tail, word, results)
       {:ready, pid} ->
         send pid, {:shutdown}
         if length(processes) > 1 do
-          schedule_processes(List.delete(processes,pid), queue, results)
+          schedule_processes(List.delete(processes,pid), queue, word, results)
         else
-          Enum.sort(results,fn {n1,_}, {n2,_} -> n1 <= n2 end)
+          results |> Enum.reduce(0, fn(x, acc) -> x + acc end)
         end
-      {:answer, number, result, _pid }->
-        schedule_processes(processes, queue, [{number, result} | results])
+      {:answer, count_result, _pid } ->
+        schedule_processes(processes, queue, word, [ count_result| results])
     end
   end
 end
 
 dir = "./files/"
-file_path_list = File.ls!(dir) |> Enum.map(fn file_name -> dir <> file_name end )
-IO.inspect(file_path_list)
+File.cd!(dir)
+file_path_list = File.ls!()
+search_word = "cat"
+
 Enum.each 1..16, fn num_processes ->
   {time, result} = :timer.tc(
     Scheduler, :run,
-    [num_processes, WordCounter, :count_word , file_path_list]
+    [num_processes, WordCounter, :count_word , file_path_list , search_word]
   )
   if num_processes == 1 do
     IO.puts inspect result
